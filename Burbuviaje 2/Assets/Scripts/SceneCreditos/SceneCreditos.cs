1840f816f27b69bd6231f4fc8f3c56ad71e05119
Ferrenz78 <enzo.ferrari@live.com.ar>
2026-01-31 11:24:54 -0300

    Estoy subiendo proyecto inicial

---
+++ Burbuviaje 2/Assets/Scripts/SceneCreditos/SceneCreditos.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCreditos : MonoBehaviour
+{
+    public void Salir()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
