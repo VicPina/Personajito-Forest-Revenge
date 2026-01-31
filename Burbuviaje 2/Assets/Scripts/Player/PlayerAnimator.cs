822b912b8100cdb86e16800ca01dcdc96b7d6a18
VicPina <vicpanzu7@gmail.com>
2026-01-31 17:30:36 -0300

    Player tweaks

--- Burbuviaje 2/Assets/Scripts/Player/PlayerAnimator.cs
+++ Burbuviaje 2/Assets/Scripts/Player/PlayerAnimator.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     public void PHTurn(Vector2 direction)
-    { 
+    {
+        if(direction.x < 0) { transform.rotation = Quaternion.Euler(Vector3.forward * 90); }
+        if(direction.x > 0) { transform.rotation = Quaternion.Euler(Vector3.forward * -90); }
+        if(direction.y < 0) { transform.rotation = Quaternion.Euler(Vector3.forward * 180); }
+        if(direction.y > 0) { transform.rotation = Quaternion.Euler(Vector3.forward * 0); }
     }
 }
--- Burbuviaje 2/Assets/Scripts/Player/PlayerController.cs
+++ Burbuviaje 2/Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     public PlayerInteractor interactor;
     public PlayerAnimator playerAnimator;
 
+    public float movementSpeed = 1.0f;
+
     private Rigidbody2D _rigidbody;
 
     private Vector2 _movementInput;
@@ -13,12 +15,27 @@ public class PlayerController : MonoBehaviour
     public void OnPlayerMovement(InputAction.CallbackContext context)
     {
         _movementInput = context.ReadValue<Vector2>();
+        if (context.performed)
+        {
+            playerAnimator.PHTurn(context.ReadValue<Vector2>());
+        }
     }
     public void OnPlayerInteraction(InputAction.CallbackContext context)
     {
         if(context.performed)
         {
             interactor.InteractWithObject();
+        }
+    }
+    public void OnMaskChange(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            Vector2 mask = context.ReadValue<Vector2>();
+            if (mask.x < 0) { print(1); }
+            if (mask.y > 0) { print(2); }
+            if (mask.y < 0) { print(3); }
+            if (mask.x > 0) { print(4); }
         }
     }
     private void Awake()
@@ -36,6 +53,6 @@ public class PlayerController : MonoBehaviour
             _rigidbody.linearVelocity = Vector2.zero;
             return;
         }
-        _rigidbody.linearVelocity = _movementInput;
+        _rigidbody.linearVelocity = _movementInput * movementSpeed;
     }
 }
