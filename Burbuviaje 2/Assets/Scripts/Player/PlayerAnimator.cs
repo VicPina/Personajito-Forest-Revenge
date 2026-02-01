98944daf9fcf1f8078c08f07215fd26917bceea0
Ferrenz78 <enzo.ferrari@live.com.ar>
2026-02-01 16:32:21 -0300

    aaaa

--- Burbuviaje 2/Assets/Scripts/Player/PlayerAnimator.cs
+++ Burbuviaje 2/Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,17 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
     private Vector2 _lastMoveDirection = Vector2.down;
     private Animator animator;
+    private int mask;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
-    public void PHTurn(Vector2 direction)
+    public void PHTurn(Vector2 direction, MaskData maskData)
     {
+        if (maskData == null)
+        {
+            mask = 0;
+        }
+        else
+        {
+            switch (maskData.maskType)
+            {
+                case maskType.None: mask = 0; break;
+                case maskType.Bosque: mask = 1; break;
+                case maskType.Pantano: mask = 2; break;
+                case maskType.Desierto: mask = 3; break;
+                case maskType.Medieval: mask = 4; break;
+            }
+        }
+        
+
+
         //Guardo el ultimo movimiento para saber para donde iba
         if (direction != Vector2.zero)
         {
@@ -20,6 +40,7 @@ public class PlayerAnimator : MonoBehaviour
         animator.SetFloat("Horizontal", _lastMoveDirection.x);
         animator.SetFloat("Vertical", _lastMoveDirection.y);
         animator.SetFloat("Velocidad", direction.magnitude);
+        animator.SetInteger("Mascara", mask);
         //sDebug.Log($"Original: {direction.magnitude}  Ultimo: {_lastMoveDirection.magnitude}");
 
         //if (direction.x < 0) { transform.rotation = Quaternion.Euler(Vector3.forward * 90); }
--- Burbuviaje 2/Assets/Scripts/Player/PlayerController.cs
+++ Burbuviaje 2/Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public PlayerAnimator playerAnimator;
     public MaskSelector maskSelector;
 
+    private MaskData maskData;
+
+   
+
     public float movementSpeed = 1.0f;
 
     private Rigidbody2D _rigidbody;
@@ -18,11 +22,13 @@ public class PlayerController : MonoBehaviour
         _movementInput = context.ReadValue<Vector2>();
         if (context.performed)
         {
-            playerAnimator.PHTurn(context.ReadValue<Vector2>());
+            
+            playerAnimator.PHTurn(context.ReadValue<Vector2>(), maskSelector.currentMask);
+            //Debug.Log(maskSelector.currentMask);
         }
         else
         {
-            playerAnimator.PHTurn(Vector2.zero);
+            playerAnimator.PHTurn(Vector2.zero, maskSelector.currentMask);
         }
     }
     public void OnPlayerInteraction(InputAction.CallbackContext context)
@@ -37,11 +43,13 @@ public class PlayerController : MonoBehaviour
         if(context.started)
         {
             maskSelector.ChooseMask(context.ReadValue<Vector2>());
+            maskData = maskSelector.currentMask; 
         }
     }
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        //maskData.maskType;
     }
     private void Update()
     {
