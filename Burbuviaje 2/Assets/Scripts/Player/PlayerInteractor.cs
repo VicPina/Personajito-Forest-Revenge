af3638443209d8129cc27b0fba7789b26534230e
Ferrenz78 <enzo.ferrari@live.com.ar>
2026-02-01 11:27:18 -0300

    Inicio listo

--- Burbuviaje 2/Assets/Scripts/Player/PlayerInteractor.cs
+++ Burbuviaje 2/Assets/Scripts/Player/PlayerInteractor.cs
@@ -3,18 +3,47 @@ using UnityEngine;
 public class PlayerInteractor : MonoBehaviour
 {
     private InteractableItem itemInRange;
+    private IInteractable interactableInRange;
+    public GameObject interactionIcon;
+
+    private void Start()
+    {
+        interactionIcon.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Interactable"))
+        if (collision.CompareTag("NPC"))
+        {
+            if (collision.TryGetComponent(out IInteractable interactable) && interactable.CanInteract())
+            {
+                interactableInRange = interactable;
+                interactionIcon.SetActive(true);
+            }
+                
+        }
+
+            if (collision.CompareTag("Interactable"))
         {
             itemInRange = collision.GetComponent<InteractableItem>();
         }
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        interactionIcon.SetActive(false);
+    }
     public void InteractWithObject()
     {
-        if(itemInRange != null)
+        interactableInRange?.Interact();
+        if (!interactableInRange.CanInteract())
         {
+            interactionIcon.SetActive(false);
+        }
+
+        if (itemInRange != null)
+        {
+
             itemInRange.onInteraction.Invoke();
         }
     }
